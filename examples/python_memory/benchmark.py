ea0dc779f12fd21cdef19d028897a17b389941ee
Kai Zhang <kylerzhang11@gmail.com>
2015-05-31 13:34:54 +0800

    Update python memory management benchmark.

--- examples/python_memory/benchmark.py
+++ examples/python_memory/benchmark.py
@@ -6,6 +6,8 @@ import resource
 import sys
 import weakref
 
+import matplotlib.pyplot as plt
+
 
 ATTRIBUTE_NAME_LIST = None
 
@@ -27,6 +29,7 @@ def parse_arguments():
 		"--unloop-model",
 		type=int,
 		choices=[0, 1, 2],
+		default=0,
 		help=(
 			"Unloop model. 0: do not unloop; 1: unloop manually;"
 			"2: unloop by weakref."))
@@ -50,10 +53,10 @@ def parse_arguments():
 		help="Monster added per dungeon.")
 	parser.add_argument(
 		"-a",
-		"--monster-data",
+		"--attribute-count",
 		type=int,
-		default=10,
-		help="The data length in a monster.")
+		default=20,
+		help="The attribute count in a monster.")
 	return parser.parse_args()
 
 
@@ -70,9 +73,9 @@ class Dungeon(object):
 
 class Monster(object):
 	ATTRIBUTE_NAME_LIST = None
-	def __init__(self, monster_data):
+	def __init__(self, attribute_count):
 		self._dungeon = None
-		for i in xrange(monster_data):
+		for i in xrange(attribute_count):
 			self.__dict__[Monster.ATTRIBUTE_NAME_LIST[i]] = i
 
 	def on_add_to_dungeon(self, dungeon):
@@ -84,50 +87,40 @@ class MonsterWithWeakref(Monster):
 		self._dungeon = weakref.ref(dungeon)
 
 
-def run_logic_with_unloop_manullay(
-		max_iteration, dungeon_per_iter, monster_per_dungeon, monster_data):
-	max_iter_time = 0
+def run_logic(
+		max_iteration, dungeon_per_iter, monster_per_dungeon, attribute_count,
+		unloop_model):
+	iter_timer_list = []
 	for i in xrange(max_iteration):
 		begin_at = time.time()
 		for dc in xrange(dungeon_per_iter):
 			dungeon = Dungeon()
 			for mc in xrange(monster_per_dungeon):
-				monster = Monster(monster_data)
+				if unloop_model == 2:
+					monster = MonsterWithWeakref(attribute_count)
+				else:
+					monster = Monster(attribute_count)
 				dungeon.add_monster(monster)
-			dungeon.destroy()
+			if unloop_model == 1:
+				dungeon.destroy()
 		end_at = time.time()
-		max_iter_time = max(max_iter_time, end_at-begin_at)
-	return max_iter_time
+		iter_timer_list.append(end_at-begin_at)
+	return iter_timer_list
 
 
-def run_logic_without_unloop(
-		max_iteration, dungeon_per_iter, monster_per_dungeon, monster_data):
-	max_iter_time = 0
-	for i in xrange(max_iteration):
-		begin_at = time.time()
-		for dc in xrange(dungeon_per_iter):
-			dungeon = Dungeon()
-			for mc in xrange(monster_per_dungeon):
-				monster = Monster(monster_data)
-				dungeon.add_monster(monster)
-		end_at = time.time()
-		max_iter_time = max(max_iter_time, end_at-begin_at)
-	return max_iter_time
-
-
-def run_logic_with_unloop_weakref(
-		max_iteration, dungeon_per_iter, monster_per_dungeon, monster_data):
-	max_iter_time = 0
-	for i in xrange(max_iteration):
-		begin_at = time.time()
-		for dc in xrange(dungeon_per_iter):
-			dungeon = Dungeon()
-			for mc in xrange(monster_per_dungeon):
-				monster = MonsterWithWeakref(monster_data)
-				dungeon.add_monster(monster)
-		end_at = time.time()
-		max_iter_time = max(max_iter_time, end_at-begin_at)
-	return max_iter_time
+def run_test_and_plot(
+		test_name, max_iteration, dungeon_per_iter, monster_per_dungeon,
+		attribute_count, unloop_model):
+	print "Run test:", test_name
+	gc.collect()
+	iter_timer_list = run_logic(
+		max_iteration,
+		dungeon_per_iter,
+		monster_per_dungeon,
+		attribute_count,
+		unloop_model)
+	plt.plot(iter_timer_list, label=test_name)
+	gc.collect()
 
 
 def main():
@@ -136,33 +129,29 @@ def main():
 		gc.disable()
 	elif arguments.gc_threshold0 is not None:
 		gc.set_threshold(arguments.gc_threshold0)
-	if arguments.unloop_model == 0:
-		benchmark_method = run_logic_without_unloop
-	elif arguments.unloop_model == 1:
-		benchmark_method = run_logic_with_unloop_manullay
-	elif arguments.unloop_model == 2:
-		benchmark_method = run_logic_with_unloop_weakref
-	else:
-		# This shouldn't happen.
-		sys.exit(1)
-	gc_model = not arguments.disable_gc
-	gc_threshold0 = gc.get_threshold()[0]
-	unloop = arguments.unloop_model
-	monster_data = arguments.monster_data
 	Monster.ATTRIBUTE_NAME_LIST = [
-		"attribute_{0}".format(i) for i in xrange(monster_data)]
-	begin_at = time.time()
-	max_iter_time = benchmark_method(
-		arguments.max_iteration,
-		arguments.dungeon_per_iter,
-		arguments.monster_per_dungeon,
-		arguments.monster_data)
-	end_at = time.time()
-	time_usage = end_at-begin_at
-	memory = resource.getrusage(resource.RUSAGE_SELF).ru_maxrss*0.001
-	print "{0},{1},{2},{3},{4},{5},{6}".format(
-		gc_model, gc_threshold0, unloop, monster_data, time_usage, memory,
-		max_iter_time)
+		"attribute_{0}".format(i) for i in xrange(arguments.attribute_count)]
+
+	test_list = [
+		("GC(threshold0:700)", 700, 0),
+		("GC(threshold0:10000)", 700, 0),
+		("Manual", None, 1),
+		("Weakref", None, 2),
+	]
+
+	for test_name, threshold, unloop_model in test_list:
+		if threshold is not None:
+			gc.set_threshold(threshold)
+		run_test_and_plot(
+			test_name,
+			arguments.max_iteration,
+			arguments.dungeon_per_iter,
+			arguments.monster_per_dungeon,
+			arguments.attribute_count,
+			unloop_model)
+	lgd = plt.legend(loc="center left", bbox_to_anchor=(1, 0.5))
+	plt.savefig(
+		"benchmark.png", bbox_extra_artists=[lgd,], bbox_inches="tight")
 
 
 if __name__ == "__main__":
